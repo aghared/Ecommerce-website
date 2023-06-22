60ebe145532fd87c30eb83edf49d63084f748256
Johnhenry Do <jdo1156@sdsu.edu>
2023-06-21 21:41:37 -0700

    Created Product Page file, images are now links to The default Product Page

---
+++ Product_Page.js
@@ -0,0 +1,8 @@
+product_name = "Product Name"
+product_image = "135.png"
+price = "$" + "23"
+product_description = "product description"
+document.getElementById("product-image").setAttribute("src", product_image)
+document.getElementById("product-name").innerHTML = product_name
+document.getElementById("price").innerHTML = price
+document.getElementById("product-description").innerHTML = product_description
