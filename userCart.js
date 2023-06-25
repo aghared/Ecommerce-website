56f9bbf8c400fdc973b2317853ed767ecbd72706
benjidaluz <111111029+benjidaluz@users.noreply.github.com>
2023-06-25 15:42:02 -0700

    added react files manually
    src folder: app.js, index.js and css, index.js, components folder
    components: checkoutpage.js, db.json, header.js, Product_page_home_style.css, shoppingcart.js, style2.css, usercart.js

---
+++ userCart.js
@@ -0,0 +1,34 @@
+import React from "react";
+import data from "./db.json";
+
+function UserCart({userId, data, deleteTask}) {
+  const user = data.users.find((user) => user.id === userId);
+  if (!user) {
+    return null;
+  }
+
+  return (
+    user.cart.map((item, index) => {
+      const product = data.products.find((product) => product.id === item.productId);
+      
+
+      if (!product) {
+        return null;
+      }
+
+      return (
+        <tr key={index}>
+          <td>{product.name}</td>
+          <td>{item.quantity}</td>
+          <td>${product.price * item.quantity}</td>
+          <td>
+            <button onClick={() => deleteTask(item.productId)}>X</button>
+          </td>
+        </tr>
+      )
+    })
+  );
+}
+
+export default UserCart;
+
