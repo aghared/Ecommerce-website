62420b6ec415b196f69d9fa317ccb9ef163b8b69
Johnhenry Do <jdo1156@sdsu.edu>
2023-06-24 23:49:32 -0700

    Images are now links to Product page

--- ecommerce-website/src/App.js
+++ ecommerce-website/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-//import './App.css';
+import './App.css';
+
+//import pages here 
 import { ProductPageHome  } from './pages/Product_Page_Home';
 import { ProductPage } from './pages/Product_Page';
+
 function App() {
   return (
   <React.Fragment>
   <nav>
     <ul>
+      {/* Add navbar link here */}
       <li><Link to="/"> Product Home</Link></li>
-      <li><Link to="/Product_Page"> Product Page</Link></li>
     </ul>
   </nav>
   <Routes>
+    {/* Add pages here*/}
     <Route path="/" element={<ProductPageHome />}/>
     <Route path="/Product_Page" element={<ProductPage/>} />
 
