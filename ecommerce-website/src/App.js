ba2f9cebebe0ea602201a395b6a26f7078965e77
Johnhenry Do <jdo1156@sdsu.edu>
2023-06-24 20:30:20 -0700

    Added React

---
+++ ecommerce-website/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+//import React, {Component} from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+//import './App.css';
+import { ProductPageHome  } from './pages/Product_Page_Home';
+import { ProductPage } from './pages/Product_Page';
+function App() {
+  return (
+  <React.Fragment>
+  <nav>
+    <ul>
+      <li><Link to="/"> Product Home</Link></li>
+      <li><Link to="/Product_Page"> Product Page</Link></li>
+    </ul>
+  </nav>
+  <Routes>
+    <Route path="/" element={<ProductPageHome />}/>
+    <Route path="/Product_Page" element={<ProductPage/>} />
+
+  </Routes>
+  </React.Fragment>
+  )
+}
+
+export default App;
---
+++ ecommerce-website/src/pages/Product_Page.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export function ProductPage(){
+    return <h1>Product Page</h1>
+}
