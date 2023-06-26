06eb83c4710c9058866af7e624b520579a2e22b2
RaniJabbar <89680382+RaniJabbar@users.noreply.github.com>
2023-06-25 21:55:48 -0700

    Modified navbar.js and Apps.js to contains SearchResults so that the search bar functions properly, and added SearchResults.js

--- ecommerce-website/src/componet/navbar.js
+++ ecommerce-website/src/componet/navbar.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import "../CSS/navbar.css"
+
 const Navbar = () => {
+    const [searchInput, newSearchInput] = useState('');
+    const navigate = useNavigate();
+  
+    const searchChange = (event) => {
+      newSearchInput(event.target.value);
+    };
+  
+    const startSearch = () => {
+      if (searchInput !== '') {
+        navigate(`/search?term=${(searchInput)}`); //used chatGPT for help on this line
+      }
+    };
+  
     return (
-        <div>
-             <div class="ecumenic">
-                 <p>eCommerce3</p>
-             </div>
-             <div class="search-bar">
-                 <input type="text" class="search-input" placeholder="Search..."/>
-                 <button class="search-button">Search</button>
-             </div>
-            <div class="navbar">
-                <Link to="/home">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/services">Services</Link>
-                <Link to="/shoppingcart">Cart</Link>
-                <Link to="/contact">Contact Us</Link>
-            </div>
+      <div>
+        <div className="ecumenic">
+          <p>eCommerce3</p>
         </div>
-    )
-       
-    
-}
-export default Navbar;
+        <div className="search-bar">
+          <input type="text" className="search-input" placeholder="Search..." value={searchInput} onChange={searchChange}
+          />
+          <button className="search-button" onClick={startSearch}>
+            Search
+          </button>
+        </div>
+        <div className="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/cart">Cart</Link>
+          <Link to="/contact">Contact Us</Link>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Navbar;
