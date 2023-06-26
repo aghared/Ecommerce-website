94f77a3f5e1b57e89e166cf0bfabe4ee2ce8e664
Johnhenry Do <jdo1156@sdsu.edu>
2023-06-26 02:22:44 -0700

    added product page stylesheet

--- ecommerce-website/src/pages/Product_Page.js
+++ ecommerce-website/src/pages/Product_Page.js
@@ -1,32 +1,39 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import product from '../data/product-data.json' 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../CSS/product-page.css'
 
 export function ProductPage(){
     const {id} = useParams()
     return (
     
         <div className="Product-Page">
-            
-            <img id={"product-image"} src ={product[id].image} alt={""}width="400px" height="400px" />
+            <div id={"image-div"}>
+                <img id={"product-image"} src ={product[id].image} alt={""}width="400px" height="400px" />
+            </div>
+
+            <div id={"info"}>
             <h2 id="product-name"> {product[id].productName} </h2>
-            <h2 id="price"> {product[id].price}</h2>
+            <h2 id="price"> {"$" + product[id].price}</h2>
             <p id={"product-description"}> {product[id].prodcutDescription}</p>
             <div className="add-cart">
                 <form >
-                    <label for="Qty"> Qty</label>
+                    <label id={"qty-lable"} for="Qty"> Qty</label>
                     <select name="Qty" id="Qty">
                         <option value="1">1</option>
                         <option value="1">2</option>
                         <option value="1">3</option>
                     </select>
                     
-                    <input type="submit" value="Add Cart "/>
+                    <div className="add-cart-btn">
+                        <Link to="/shoppingcart" className="btn btn-success">Add Cart</Link>
+                    </div>
                 </form>
            
             
             </div>
-        
+            </div>
         </div>
         
     
