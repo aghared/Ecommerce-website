f6699804555aa966ba541c0c72b5bfe7b9186b53
RaniJabbar <89680382+RaniJabbar@users.noreply.github.com>
2023-06-27 19:26:23 -0700

    Included full functionality for the search bar. Search bar will now filter items based on the product-data.json productName tag.

--- ecommerce-website/src/componet/SearchResults.js
+++ ecommerce-website/src/componet/SearchResults.js
@@ -8,19 +8,26 @@ const SearchResults = () => {
   const searchParams = new URLSearchParams(location.search); //used ChatGPT for help on these 3 lines
   const searchTerm = searchParams.get('term');
 
+  //product filter
+  const filteredProducts = product.filter( item =>
+    item.productName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Search Results for "{searchTerm}"</h2>
         <div className ="scroll-container">
             <div id="scroll-container-Recomendation">
                 {
-                    product.map(item => { 
-                            return(
-                                <Link to={"/Product_Page/"+item.id}><img src={item.image} alt={""}/></Link >
-                            )
-
-                        } 
-                    )
+                  filteredProducts.length === 0 ? (
+                    <p>No items match your search</p>
+                  ) :
+                  filteredProducts.map(item => {
+                    return(
+                      <Link to={"/Product_Page/"+item.id}><img src={item.image} alt={""}/></Link >       
+                      )
+                    }
+                  )
 
                 }
                             
