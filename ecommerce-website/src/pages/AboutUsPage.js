7bc199dac4a04faa7e97303de1207d77ee6a5168
aghared <aghashaba@gmail.com>
2023-06-28 15:43:49 -0700

    added about us  page

---
+++ ecommerce-website/src/pages/AboutUsPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const AboutUsPage = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Welcome to our eCommerce store! We specialize in providing high-quality food diet products to support your healthy lifestyle.
+      </p>
+      <p>
+        Our mission is to offer a diverse range of products that cater to your specific dietary needs and preferences. Whether you're following a vegan, gluten-free, or keto diet, we have a wide selection of options available for you.
+      </p>
+      <p>
+        At our eCommerce store, we are committed to promoting a healthy and balanced diet. We carefully curate our product selection to ensure that you have access to nutritious and delicious food choices.
+      </p>
+      <p>
+        Customer satisfaction is our top priority. We have a dedicated support team ready to assist you with any questions or concerns you may have about our products. Your feedback is invaluable to us as we continually strive to improve our offerings and services.
+      </p>
+      <p>
+        Thank you for choosing our eCommerce store for your food diet needs. We look forward to being a part of your journey towards a healthier lifestyle.
+      </p>
+    </div>
+  );
+};
+
+// export default AboutUsPage;
