7e85793dbda1d5719ab477fca2c71bb5c57e971f
benjidaluz <benji.daluz@gmail.com>
2023-06-29 01:56:15 -0700

    added user cart to models

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,5 +7,12 @@ class Product(models.Model):
     productPrice = models.IntegerField()
     productDescription = models.CharField(max_length=255)
 
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
 
-    
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
