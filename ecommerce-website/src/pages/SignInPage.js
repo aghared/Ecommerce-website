c9e4d35aea0e6cb82dd9438dff78880865e1d752
benjidaluz <benji.daluz@gmail.com>
2023-06-29 22:03:16 -0700

    fixed sign in page
    moved sign in box to middle

--- ecommerce-website/src/pages/SignInPage.js
+++ ecommerce-website/src/pages/SignInPage.js
@@ -7,26 +7,28 @@ export function SignInPage(){
   // sign in
 
     return (
-        <div>
-        <br />
-        <br />
-        <aside className="aside-bar">
-        <h2 id="sign">Sign In</h2>
-        <form id="signup-form">
-          <label id="user" htmlFor="username">Username:</label>
-          <input type="text" id="input" required />
-  
-          <label id="user" htmlFor="password">Password:</label>
-          <input type="password" id="input" required />
-  
-          <button id="butt" type="submit">Sign in</button>
-          <div className="signup-link">
-            <p>
-              Don't have an account? <Link to="/signup" id="signup-link">Sign Up</Link>
-            </p>
+      <div className="middle-align">
+        <div className="signin-middle-align">
+            <div>
+            <aside className="aside-bar">
+            <h2 id="sign">Sign In</h2>
+            <form id="signup-form">
+              <label id="user" htmlFor="username">Username:</label>
+              <input type="text" id="input" required />
+      
+              <label id="user" htmlFor="password">Password:</label>
+              <input type="password" id="input" required />
+      
+              <button id="butt" type="submit">Sign in</button>
+              <div className="signup-link">
+                <p>
+                  Don't have an account? <Link to="/signup" id="signup-link">Sign Up</Link>
+                </p>
+              </div>
+            </form>
+          </aside>
           </div>
-        </form>
-      </aside>
+        </div>
       </div>
 
     )
