c73c845e88b867832a725de942832d431a0681db
benjidaluz <benji.daluz@gmail.com>
2023-07-02 22:15:24 -0700

    Revert "update sign page"
    This reverts commit febc2880dad8fac10555237a2de03f817e90d0a4.

--- ecommerce-website/src/pages/RegistrationPage.js
+++ ecommerce-website/src/pages/RegistrationPage.js
@@ -1,63 +1,24 @@
 import React from 'react';
-import { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../CSS/sign-page.css';
 
 export const RegistrationPage = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [dietPlan, setDietPlan] = useState('cutting');
-  const history = useHistory();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      // Create an object with the form data
-      const formData = {
-        name,
-        email,
-        password,
-        dietPlan,
-      };
-  
-      // Send a POST request to your backend API with the form data
-      const response = await fetch('/api/signup', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-  
-      if (response.ok) {
-        // Form submission successful, you can redirect the user to the desired page
-        history.push('/home');
-      } else {
-        // Form submission failed, handle the error accordingly
-        console.error('Form submission failed');
-      }
-    } catch (error) {
-      console.error('An error occurred during form submission:', error);
-    }
-  };
-
   return (
     <div>
       <div className="registration-form">
         <h2>Create an Account</h2>
-        <form id="registration-form" onSubmit={handleSubmit}>
+        <form id="registration-form">
           <label htmlFor="name">Name:</label>
-          <input type="text" id="name" required value={name} onChange={(e) => setName(e.target.value)} />
+          <input type="text" id="name" required />
 
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="email" id="email" required />
 
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input type="password" id="password" required />
 
           <label htmlFor="diet-plan">Preferred Diet Plan:</label>
-          <select id="diet-plan" value={dietPlan} onChange={(e) => setDietPlan(e.target.value)}>
+          <select id="diet-plan">
             <option value="cutting">Cutting</option>
             <option value="bulking">Bulking</option>
             <option value="none">None</option>
